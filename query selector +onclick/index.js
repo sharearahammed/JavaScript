5c3779a1b10ae721235d98ffe23373de5be9e8fb
Sharear Ahammed Nihal <106548719+sharearahammed@users.noreply.github.com>
2023-08-07 15:13:03 +0600

    Add files via upload

---
+++ query selector +onclick/index.js
@@ -0,0 +1,28 @@
+const myheading = document.getElementById("heading");
+myheading.innerHTML="LEO";
+const mypara1 = document.getElementById("para");
+mypara1.innerHTML="Lionel Messi is a football player from Argentina who plays for FC Barcelona.";
+
+
+const mypara2 = document.getElementById("paraID");
+
+function myMessage1(){
+    mypara2.innerHTML="This in button 1";
+}
+function myMessage2(){
+    mypara2.innerHTML="This in button 2";
+}
+
+
+
+
+
+
+
+const myImg = document.getElementById("image");
+function mypicture1(){
+    myImg.src="images/messi.jpg";
+}
+function mypicture2(){
+    myImg.src="images/naymer.jpeg";
+}
