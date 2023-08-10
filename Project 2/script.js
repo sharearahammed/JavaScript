7904a8b4fc86697f3f351bb7beb2287b681cc11a
Sharear Ahammed Nihal <106548719+sharearahammed@users.noreply.github.com>
2023-08-10 21:54:23 +0600

    Add files via upload

---
+++ Project 2/script.js
@@ -0,0 +1,42 @@
+const posts = [
+    {
+        title : "this is title 1",
+        body : "this is body 1"
+    },
+    {
+        title : "this is title 2",
+        body : "this is body 2"
+    },
+    {
+        title : "this is title 3",
+        body : "this is body 3"
+    },
+    {
+        title : "this is title 4",
+        body : "this is body 4"
+    },
+    {
+        title : "this is title 5",
+        body : "this is body 5"
+    },
+    {
+        title : "this is title 6",
+        body : "this is body 6"
+    },
+    {
+        title : "this is title 7",
+        body : "this is body 7"
+    },
+    {
+        title : "this is title 8",
+        body : "this is body 8"
+    }
+];
+
+
+/* <div class="post">
+            <h5>post title 1</h5>
+            <p>post description</p>
+        </div> */
+
+        const postsElement = document.querySelector(".posts");
